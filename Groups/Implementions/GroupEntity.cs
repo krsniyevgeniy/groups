370999863392244069529cc89b576ace9b3b4930
Eugen Krasniy <krsniyevgeniy@gmail.com>
2024-04-27 08:23:05 +0300

    Implement models.

--- Groups/Implementions/GroupEntity.cs
+++ Groups/Implementions/GroupEntity.cs
@@ -4,5 +4,17 @@ namespace Groups.Implementions;
 
 public class GroupEntity
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CreatedYear { get; set; }
+    public List<StudentEntity> Students { get; set; }
+
+    public GroupEntity(int id, string name, int year)
+    {
+        Id = id;
+        Name = name;
+        CreatedYear = year;
+        Students = new List<StudentEntity>();
+    }
 
 }
